16f2bb13a41a5b8fb452b9d1861f076e279715a5
Seth Bashford <seth.bashford@boystown.org>
2023-04-07 15:58:27 -0500

    better

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
-use nifti::{IntoNdArray, NiftiObject, NiftiVolume, ReaderOptions};
+use nifti::{IntoNdArray, NiftiObject};
 use std::io::Write;
 
 fn main() -> Result<(), std::io::Error> {
-    let nifti = ReaderOptions::new()
-        .read_file("a.nii")
+    let input_file_path = "in.nii";
+    let nifti = nifti::ReaderOptions::new()
+        .read_file(input_file_path)
         .expect("Should be able to read nifti file.");
     let slices = nifti
         .volume()
         .into_ndarray::<f32>()
         .expect("Should be able to convert volume into ndarray.");
-    let mut file = std::io::BufWriter::new(
-        std::fs::File::create("out.dat").expect("Should be able to create a file."),
-    );
-    writeln!(file,
+    println!("{:?}", nifti.header());
+    let mut file = std::io::BufWriter::new(std::fs::File::create("out.dat")?);
+    writeln!(
+        file,
+        "BESA_SA_IMAGE:2.0
+
+Data file:          {input_file_path}
+Condition:          Correct  Notch filter: 60 Hz
+Correct 350 : 650 ms 8.0-14.0 Hz MSBF (TF) q%
+
+Grid dimensions ([min] [max] [nr of locations]):
+X: {x_min:.6} 70.000000 36
+Y: {y_min:.6} 71.629997 45
+Z: {z_min:.6} 77.379997 35",
+        x_min = nifti.header().srow_x[3],
+        y_min = nifti.header().srow_y[3],
+        z_min = nifti.header().srow_z[3]
+    )?;
+    write!(file,
 "=============================================================================================="
     )?;
     for (z_index, slice) in slices.axis_iter(ndarray::Axis(2)).enumerate() {
+        writeln!(file)?;
         writeln!(file, "Z: {}", z_index)?;
-        for row in slice.columns() {
-            if let Some(first) = row.first() {
+        for column in slice.columns() {
+            if let Some(first) = column.first() {
                 write!(file, "{:.10}", first)?;
             }
-            for sample in row.iter().skip(1) {
+            for sample in column.iter().skip(1) {
                 write!(file, " {:.10}", sample)?;
             }
             writeln!(file)?;
         }
-        writeln!(file)?;
     }
     Ok(())
 }
